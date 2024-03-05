f3987d89bfc164c3c0cdd4c60764fe8384256746
daveprowse <dprowseus@gmail.com>
2024-03-05 11:46:18 -0500

    3-5-2024-C

--- z-more-info/k8s-scripts/kubetools-install.sh
+++ z-more-info/k8s-scripts/kubetools-install.sh
@@ -25,7 +25,7 @@ EOF
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
+deb https://apt.kubernetes.io/kubernetes-xenial main
 EOF
 
 sudo apt-get update
