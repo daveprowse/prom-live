2f449a86139cd7c28613b39ccfafb78cc706f724
daveprowse <dprowseus@gmail.com>
2024-03-05 12:00:55 -0500

    3-5-2024-F

--- z-more-info/k8s-scripts/kubetools-install.sh
+++ z-more-info/k8s-scripts/kubetools-install.sh
@@ -25,6 +25,7 @@ EOF
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 sudo mkdir -p -m 755 /etc/apt/keyrings
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
