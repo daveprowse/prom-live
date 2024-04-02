433f0a1e26fac4d65674052d5f948eb77968091c
daveprowse <dprowseus@gmail.com>
2024-04-02 12:20:02 -0400

    04-02-2024 | 12:20:02 PM

--- git-push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-## Pushes all content automatically and gives timestamp for date
-
-echo 'Running Script!'
-
-git add .
-git commit -m "$(date '+%m-%d-%Y | %r')"
-git push -u origin main
