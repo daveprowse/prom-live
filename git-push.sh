bcf2c11bfb5f7b3c25792975c47688a6fb05b86f
daveprowse <dprowseus@gmail.com>
2024-04-02 12:00:36 -0400

    $(x)

--- git-push.sh
+++ git-push.sh
@@ -6,7 +6,7 @@ echo 'Running Script!'
 x=$(date -I)
 
 echo "$(git add .)"
-echo "$(git commit -m '`(date -I)`')"
+echo "$(git commit -m '$(x)')"
 echo "$(git push -u origin main)"
 
 
@@ -15,3 +15,4 @@ testedte
 test2
 test3
 test4
+test5
